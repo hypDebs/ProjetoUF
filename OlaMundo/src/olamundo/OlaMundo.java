68c23c154424682d4e059e4e203c292eaf241489
Debora Freire <debora.py01@hotmail.com>
2018-01-29 15:44:36 -0200

    Pegunta nome

--- OlaMundo/src/olamundo/OlaMundo.java
+++ OlaMundo/src/olamundo/OlaMundo.java
@@ -1,7 +1,15 @@
 package olamundo;
+
+import java.util.Scanner;
+
 public class OlaMundo {
     public static void main(String[] args) {
-        System.out.println("Ola, mundo!");
+        Scanner tec = new Scanner(System.in);
+        String nome;
+        System.out.println("Digite seu nome");
+        nome = tec.nextLine();
+        System.out.println("Ola, "+nome);
+        
     }
     
 }
